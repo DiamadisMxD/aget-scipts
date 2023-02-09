da5fe6c85e03878b2fde05436a29b58840977652
DiamadisMxD <35338011+DiamadisMxD@users.noreply.github.com>
2023-02-09 13:15:19 +0200

    skm.sh
    Running for secret key managment.

---
+++ skm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Secret key managment#
+ssh-keygen -t rsa -f ~/.ssh/private_key -C agent-cn-app -b 2048
+
+#Update secret key
+az keyvault secret set --name vmKey --vault-name kv-cn-app --file ~/.ssh/private_key
